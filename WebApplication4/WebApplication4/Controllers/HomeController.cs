c6536a42e192e0f7e4b15bd36cca70d568502741
Micky Yu <mickyyu96@gmail.com>
2016-04-15 04:11:44 +0700

    udah bisa keluar di form, belum bisa di result

--- WebApplication4/WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/WebApplication4/Controllers/HomeController.cs
@@ -6,55 +6,86 @@ using System.Web.Mvc;
 using TweetSharp;
 
 namespace WebApplication2.Controllers
-{
+{  
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult About()
         {
             return View();
         }
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
-        
-        public ActionResult Result()
-        {
-            string consumerKey = "lfwH9auYQcCb3Ba0R752Yw";
-            string consumerSecret = "EERQE8Ys3nvGThh9mTQ88w2bUIzoOLnRhvKsbOjhc";
-            string accessToken = "50680924-yIdW5ULk98ThsDm0DTQlbRXREawVMcPytQ8WS04GW";
-            string accessTokenSecret = "4fJYy6Yttdl314F5WeOTkuR8apkTFXj5ZK2MmvLtk";
-            string tweetSearchQuery = (string)TempData["Tweetsearch"];
-            var service = new TwitterService(consumerKey, consumerSecret);
-            service.AuthenticateWith(accessToken, accessTokenSecret);
-
-            IEnumerable<TwitterStatus> tweets = service.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions { ScreenName = tweetSearchQuery, Count = 5 });
-
-            //TwitterSearchResult hasil = service.Search(new SearchOptions{ Q = txtTwitterName, Count = 5 });
-            //IEnumerable<TwitterStatus> tweets = hasil.Statuses;
-            //TwitterStatus tweetStat = tweets.ElementAt(1);
-            ViewBag.PDAM = (string)TempData["QueryPDAM"];
-            ViewBag.PJU = (string)TempData["QueryPJU"];
-            ViewBag.Dinsos = (string)TempData["QueryDinsos"];
-            ViewBag.Tweets = tweets;
-            return View();
-        }
-
         [HttpGet]
         public ActionResult Form()
         {
             return View();
         }
-
+        [HttpGet]
+        public ActionResult Result()
+        {
+            return View();
+        }
         [HttpPost]
         public ActionResult Form(string txtTwitterName, string txtboxPDAM, string txtboxPJU, string txtboxDinsos)
         {
+            /*
             TempData["QueryPDAM"] = txtboxPDAM;
             TempData["QueryPJU"] = txtboxPJU;
             TempData["QueryDinsos"] = txtboxDinsos;
             TempData["Twittersearch"] = txtTwitterName;
 
             return RedirectToAction("Result");
+            */
+            string consumerKey = "DShcBlKUwZ3mUBRt3tjqJEDnd";
+            string consumerSecret = "GxJYqCJTcJyteeqSDg0sXC3xRxe4ydbbSljbSxfUvv7Z9TiROq";
+            string accessToken = "2535482743-roYx8Hbx0qqdluKQFeIQlVOKvmp5IVUXaO5z9b3";
+            string accessTokenSecret = "OGuX0xXjB9BiEw1K3Htgpm2LbeRUsh2EqBRf3k7TXi4VT";
+
+            string tweetSearchQuery = (string)TempData["Tweetsearch"];
+            var service = new TwitterService(consumerKey, consumerSecret);
+            service.AuthenticateWith(accessToken, accessTokenSecret);
+
+            IEnumerable<TwitterStatus> tweets = service.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions { ScreenName = "@PemkotBandung", Count = 5, });
+
+            //TwitterSearchResult hasil = service.Search(new SearchOptions { Q = txtTwitterName, Count = 5 });
+            //IEnumerable<TwitterStatus> tweets = hasil.Statuses;
+            //TwitterStatus tweetStat = tweets.ElementAt(1);
+
+            ViewBag.PDAM = (string)TempData["QueryPDAM"];
+            ViewBag.PJU = (string)TempData["QueryPJU"];
+            ViewBag.Dinsos = (string)TempData["QueryDinsos"];
+            ViewBag.Tweets = tweets;
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Result(string aaa)
+        {
+            string consumerKey = "DShcBlKUwZ3mUBRt3tjqJEDnd";
+            string consumerSecret = "GxJYqCJTcJyteeqSDg0sXC3xRxe4ydbbSljbSxfUvv7Z9TiROq";
+            string accessToken = "2535482743-roYx8Hbx0qqdluKQFeIQlVOKvmp5IVUXaO5z9b3";
+            string accessTokenSecret = "OGuX0xXjB9BiEw1K3Htgpm2LbeRUsh2EqBRf3k7TXi4VT";
+
+            string tweetSearchQuery = (string)TempData["Tweetsearch"];
+            var service = new TwitterService(consumerKey, consumerSecret);
+            service.AuthenticateWith(accessToken, accessTokenSecret);
+
+            IEnumerable<TwitterStatus> tweets = service.ListTweetsOnUserTimeline(new ListTweetsOnUserTimelineOptions { ScreenName = "@MickyYuu96", Count = 5, });
+
+            //TwitterSearchResult hasil = service.Search(new SearchOptions { Q = txtTwitterName, Count = 5 });
+            //IEnumerable<TwitterStatus> tweets = hasil.Statuses;
+            //TwitterStatus tweetStat = tweets.ElementAt(1);
+
+            ViewBag.PDAM = (string)TempData["QueryPDAM"];
+            ViewBag.PJU = (string)TempData["QueryPJU"];
+            ViewBag.Dinsos = (string)TempData["QueryDinsos"];
+            ViewBag.Tweets = tweets;
+
+            return View();
         }
     }
 }
